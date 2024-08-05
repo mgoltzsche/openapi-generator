371a3e5647ff3265878f9ec2833a58ce2d62ab57
Max Goltzsche <max.goltzsche@gmail.com>
2024-08-05 23:27:29 +0200

    [python-fastapi] support pydantic v2 models
    Previously, the generated `additional_properties` field showed up within
    the response of the generated API as opposed marshaling the model so that its fields are added to the root object.
    Apparently that is because pydantic v2 does not honour the generated `to_dict` methods anymore (which would have mapped the object to the correct representation) but, instead, supports additional properties natively by specifying `extra=allow` within the `model_config`.
    
    Correspondingly, the following changes have been applied:
    * To allow additional fields, specify `extra=allow` within the `model_config`.
    * Don't generate the `additional_properties` field - users can use pydantic's built-in `model.extra_fields` instead.
    * Let the `{to|from}_{dict|json}` methods delegate to Pydantic's `model_dump[_json]` methods.

--- samples/server/petstore/python-fastapi/src/openapi_server/models/tag.py
+++ samples/server/petstore/python-fastapi/src/openapi_server/models/tag.py
@@ -41,38 +41,22 @@ class Tag(BaseModel):
         "protected_namespaces": (),
     }
 
-
     def to_str(self) -> str:
         """Returns the string representation of the model using alias"""
         return pprint.pformat(self.model_dump(by_alias=True))
 
     def to_json(self) -> str:
         """Returns the JSON representation of the model using alias"""
-        # TODO: pydantic v2: use .model_dump_json(by_alias=True, exclude_unset=True) instead
-        return json.dumps(self.to_dict())
+        return self.model_dump_json(by_alias=True)
 
     @classmethod
     def from_json(cls, json_str: str) -> Self:
         """Create an instance of Tag from a JSON string"""
-        return cls.from_dict(json.loads(json_str))
+        return cls.parse_obj(json.loads(json_str))
 
     def to_dict(self) -> Dict[str, Any]:
-        """Return the dictionary representation of the model using alias.
-
-        This has the following differences from calling pydantic's
-        `self.model_dump(by_alias=True)`:
-
-        * `None` is only added to the output dict for nullable fields that
-          were set at model initialization. Other fields with value `None`
-          are ignored.
-        """
-        _dict = self.model_dump(
-            by_alias=True,
-            exclude={
-            },
-            exclude_none=True,
-        )
-        return _dict
+        """Return the dictionary representation of the model using alias"""
+        return self.model_dump(by_alias=True)
 
     @classmethod
     def from_dict(cls, obj: Dict) -> Self:
@@ -83,10 +67,6 @@ class Tag(BaseModel):
         if not isinstance(obj, dict):
             return cls.model_validate(obj)
 
-        _obj = cls.model_validate({
-            "id": obj.get("id"),
-            "name": obj.get("name")
-        })
-        return _obj
+        return cls.parse_obj(obj)
 
 
